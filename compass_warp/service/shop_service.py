337ab1ab7b59dad9645fdb9d0b2ac541afca2aaf
Liu Qi <liuqi@zhinanzhenx.com>
2020-06-01 15:16:48 +0800

    404页面调通

--- compass_warp/service/shop_service.py
+++ compass_warp/service/shop_service.py
@@ -1,16 +1,23 @@
-from compass_warp.db_models.comm_info.shop_info_db import ShopInfoDB
+# from compass_warp.db_models.comm_info.shop_info_db import ShopInfoDB
+# from shop_infos.shop_auth_db import ShopAuthDB
+
+
+# class ShopService:
+#     def __init__(self, sid, nick, platform, softname):
+#         self.sid = sid
+#         self.nick = nick
+#         self.platform = platform
+#         self.softname = softname
+#         self.shop_auth_db = ShopAuthDB(sid, nick, platform, soft_name)
+
+#     def save_shop_auth(self, uid, sub_nick, access_token, refresh_token, access_expires, refresh_expires):
+#         self.shop_auth_db.insert_shop_auth(uid, sub_nick, access_token, refresh_token, access_expires, refresh_expires)
+
+#     def get_shop_auths(self):
+#         shop_infos = self.shop_auth_db.get_shop_info_by_nick()
+#         return shop_infos
 
 
 class ShopService:
-    def __init__(self, nick, platform, softname):
-        self.nick = nick
-        self.platform = platform
-        self.softname = softname
-        self.shop_info_db = ShopInfoDB(nick, platform, softname)
-
-    def save_shop_info(self, uid, sub_nick, access_token, refresh_token, access_expires, refresh_expires):
-        self.shop_info_db.save_shop_info(uid, sub_nick, access_token, refresh_token, access_expires, refresh_expires)
-
-    def get_shop_infos(self):
-        shop_infos = self.shop_info_db.get_shop_info_by_nick()
-        return shop_infos
+    def __init__(self):
+        pass
--- compass_warp/view/main/view.py
+++ compass_warp/view/main/view.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 import logging
 
 
-def handler404(request, exception):
+def handler404(request, exception=""):
     return render(request, "404.html", {"except_msg": str(exception)})
 
 
